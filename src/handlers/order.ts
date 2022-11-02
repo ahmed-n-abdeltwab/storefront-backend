342d8df9623f3376887ebd7a10d24655695e06a9
ahmed nasser <ahmed.n.abdeltwab@gmail.com>
2022-11-03 00:56:21 +0200

    adding update handler

--- src/handlers/order.ts
+++ src/handlers/order.ts
@@ -21,7 +21,7 @@ export const index = async (
 
 export const show = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		const order: Order = await store.show(req.body.id);
+		const order: Order = await store.show(req.params.id);
 		res.json(order);
 	} catch (error) {
 		next(new HttpException(400, (error as Error).message));
@@ -47,13 +47,33 @@ export const create = async (
 	}
 };
 
+export const update = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const order: Order = {
+		id: parseInt(req.params.id),
+		product_id: req.body.product_id,
+		quantity: req.body.quantity,
+		user_id: req.body.user_id,
+		status: req.body.status,
+	};
+	try {
+		const updated: Order = await store.update(order);
+		res.status(201).json(updated);
+	} catch (error) {
+		next(new HttpException(404, (error as Error).message));
+	}
+};
+
 export const destroy = async (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
 	try {
-		const deleted: Order = await store.delete(req.body.id);
+		const deleted: Order = await store.delete(req.params.id);
 		res.json(deleted);
 	} catch (error) {
 		next(new HttpException(400, (error as Error).message));
