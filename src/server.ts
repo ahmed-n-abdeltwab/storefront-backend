dff16c6102ad945f404466b9eda8eb676dee6215
ahmed nasser <ahmed.n.abdeltwab@gmail.com>
2022-11-02 22:19:16 +0200

    adding the middlewares and routers

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,26 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express';
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+// import the routes
+import userRouter from './routers/user';
 
-app.use(bodyParser.json())
+// import the middlewares
+import invalidPathHandler from './middlewares/not-found.middleware';
+import errorMiddleware from './middlewares/error.middleware';
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+const app: express.Application = express();
+const address: string = '0.0.0.0:3000';
+
+app.use(express.json());
+
+app.use('/users', userRouter);
+
+app.get('/', function (_req: Request, res: Response) {
+	res.send('Hello World!');
+});
+
+app.use(errorMiddleware);
+app.use(invalidPathHandler);
 
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+	console.log(`starting app on: ${address}`);
+});
