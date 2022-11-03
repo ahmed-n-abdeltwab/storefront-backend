c13fd91e837c70e13f72b77367b520d0f59094d7
ahmed nasser <ahmed.n.abdeltwab@gmail.com>
2022-11-03 20:28:11 +0200

    make the username UNIQUE

--- migrations/sqls/20221031190646-users-table-up.sql
+++ migrations/sqls/20221031190646-users-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Users(
     id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL UNIQUE,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     password VARCHAR NOT NULL
