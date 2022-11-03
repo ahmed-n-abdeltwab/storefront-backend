538f55a8b7e45dfc69baeca59c9675b4dd8e705f
ahmed nasser <ahmed.n.abdeltwab@gmail.com>
2022-11-03 12:00:56 +0200

    adding : a relation betwen orders and products

--- migrations/sqls/20221031192845-order-table-up.sql
+++ migrations/sqls/20221031192845-order-table-up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE Orders(
     id SERIAL PRIMARY KEY,
-    product_id INTEGER NOT NULL,
+    product_id bigint NOT NULL,
     quantity INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
+    user_id bigint NOT NULL,
     status VARCHAR(20) NOT NULL
 );
 ALTER TABLE Orders
     ADD CONSTRAINT fk_orders_users FOREIGN KEY (user_id) REFERENCES Users(id);
+ALTER TABLE Orders
+    ADD CONSTRAINT fk_orders_product FOREIGN KEY (product_id) REFERENCES Products(id);
