f46ae7d2c624e339a1040229342d1bbf131a9a36
ahmed nasser <ahmed.n.abdeltwab@gmail.com>
2022-11-05 14:20:07 +0200

    add logger

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,8 @@
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream('access.log', { flags: 'a' })
+ 
+// setup the logger
+export default morgan('combined', { stream: accessLogStream })
