58c89e511703296d25ef605dadee9fa228a48e2d
ahmed nasser <ahmed.n.abdeltwab@gmail.com>
2022-11-05 19:50:09 +0200

    change column types

--- migrations/sqls/20221031192845-order-table-up.sql
+++ migrations/sqls/20221031192845-order-table-up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Orders(
     id SERIAL PRIMARY KEY,
-    product_id bigint NOT NULL,
+    product_id INTEGER NOT NULL,
     quantity INTEGER NOT NULL,
-    user_id bigint NOT NULL,
+    user_id INTEGER NOT NULL,
     status VARCHAR(20) NOT NULL
 );
 ALTER TABLE Orders
